d40398e52905027d7260d62329e969ebebe1921d
DEG-171 <deg.cco.171@gmail.com>
2017-06-14 19:40:20 -0300

    Create GerenciadorDePessoas.java

--- GerenciadorDePessoas.java
+++ GerenciadorDePessoas.java
@@ -3,19 +3,20 @@ public class GerenciadorDePessoa {
 	int numMaxDePessoas = 20;
 	Pessoa[] pessoas = new Pessoa[numMaxDePessoas];
 	
-	public void cadastrar_pessoa(Pessoa pessoas){
+	public void cadastrar_pessoa(Pessoa pessoa){
 		for (int i = 0; i < numMaxDePessoas; i++) {
-			if (pessoas[i].eventos_iguais(pessoas)) {
+			if (pessoas[i].pessoas_iguais(pessoa)) {
 				return;
 			}
 		}
 		for (int j = 0; j < numMaxDePessoas; j++) {
-			if (pessoas[j].getNome() == null) {
-				pessoas[j] = pessoas;
+			if (pessoas[j].getNome().equals(null)) {
+				pessoas[j] = pessoa;
 				return;
 			}
 		}
 	}
+	
 
 
 }
