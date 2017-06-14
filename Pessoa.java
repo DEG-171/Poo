c6491c72e1fce21d02cae8e69819324d8ae29157
DEG-171 <deg.cco.171@gmail.com>
2017-06-14 19:31:55 -0300

    Create Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,16 +1,33 @@
 public class Pessoa {
 	// Atributos
 	String nome;
+	int numMaxDeEventos = 20;
 	int cpf;
 	int idade;
 	Evento[] eventosComparecer;
 
 	// Cadastrar nova pessoa
 	public Pessoa(int cpf, String nome, int idade) {
+		eventosComparecer = new Evento[numMaxDeEventos];
+
 		this.nome = nome;
 		this.cpf = cpf;
 		this.idade = idade;
 
+	}
+
+	public void eventos_desejados(Evento evento) {
+		for (int i = 0; i < numMaxDeEventos; i++) {
+			if (eventosComparecer[i].eventos_iguais(evento)) {
+				return;
+			}
+		}
+		for (int j = 0; j < numMaxDeEventos; j++) {
+			if (eventosComparecer[j].getNome() == null) {
+				eventosComparecer[j] = evento;
+				return;
+			}
+		}
 	}
 
 	// Getters and Setters
@@ -21,7 +38,6 @@ public class Pessoa {
 	public void setNome(String novonome) {
 		this.nome = novonome;
 	}
-
 
 	public int getIdade() {
 		return idade;
