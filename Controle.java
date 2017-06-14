729028cdf5c4964d3809430dad07cdd2a2b67934
DEG-171 <deg.cco.171@gmail.com>
2017-06-13 21:47:56 -0300

    Create Controle.java

--- Controle.java
+++ Controle.java
@@ -4,6 +4,23 @@ public class Controle {
 
 	public static void main(String[] args) {
 		
+		switch (opcao_evento) {
+			case 1:
+				criar_evento();
+				break;
+			case 2:
+				editar_evento();
+				break;
+			case 3:
+				excluir_evento();
+				break;
+			case 4:
+				consultar_evento();
+				break;
+			case 5:
+				mainMenu();
+				break;
+			}
 		
 	}
 	
