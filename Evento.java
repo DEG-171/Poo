894ee4cba86ef65b117b9737e2c4b56921dd739a
DEG-171 <deg.cco.171@gmail.com>
2017-06-14 17:44:30 -0300

    Create Evento.java

--- Evento.java
+++ Evento.java
@@ -2,11 +2,10 @@ public class Evento {
 
 	//Atributos
 	private double preco;
-	private int[] data = new int[3]
+	private int[] data = new int[3];
 	private String local;
 	private String nome;
 	private String autor;
-	
 
 	//Construtor
 	public Evento(double preco, int[] data, String local, String nome,String autor) {
@@ -20,6 +19,8 @@ public class Evento {
 		this.data[1] = data[1];
 		this.data[2] = data[2];
 	}
+	
+	//Comparador de eventos
 	public boolean eventos_iguais(Evento evento){
 		if(
 		(this.nome.equals(evento.nome))&&
@@ -29,10 +30,20 @@ public class Evento {
 		(this.data.equals(evento.data))
 		) {
 			return true;
-		else{
+		}else{
 			return false;
 			}
 		}
+	
+	//Metodo para excluir evento
+	public void excluir_evento(double preco, int[] data, String local, String nome,String autor){
+		this.preco = 0;
+		this.local = "";
+		this.nome = "";
+		this.autor = "";
+		this.data[0] = 00;	//dia
+		this.data[1] = 00;	//mes
+		this.data[2] = 0000;	//ano
 	}
 	
 	//Getters and Setters
